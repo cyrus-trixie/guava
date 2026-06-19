d26776de88609458a8bbc05bf91a32df059ca60f
Ore-wa-Triple-M <machariamorris977@gmail.com>
2026-06-20 01:02:52 +0300

    typescript issue fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import HeroSection from "./components/Hero";
 import Header from "./components/Header";
 import AboutSection from "./components/About";
 //import Services from "./components/Services";
-import PortfolioCarousel from "./components/PortfolioCarousel"; 
+//import PortfolioCarousel from "./components/PortfolioCarousel"; 
 import RecentWork from "./components/RecentWork";
 import Footer from "./components/Footer";
 export default function Home() {
